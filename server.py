3544dd434c77425994a6c6695a973a8ab91836ab
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-26 12:19:53 -0700

    Added Login Screen and helpful links on Homepage

--- server.py
+++ server.py
@@ -55,14 +55,25 @@ def show_form_results():
     print current_user
 
     if current_user == []:
-        # add them to database
+
+        # add the new user to database
+        new_user = User(email=email, password=password, age=age, zipcode=zipcode)
+        db.session.add(new_user)
+        db.session.commit()
+
         flash("Welcome new super-rater of super-movies. You're SUPER.")
-        print "\n\n\n\nTHIS USER IS NEW\n\n\n\n"
+        print "\n\n\n\nTHIS USER IS NEW %s is IN THE DATABASE???\n\n\n\n" % new_user
         return redirect("/")
     else:
         flash("Welcome back!  You're logged in!")
         print "\n\n\n\nTHIS USER IS IN\n\n\n\n"
         return redirect("/")
+
+@app.route('/login')
+def login_form():
+    """Display Login Form"""
+
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
